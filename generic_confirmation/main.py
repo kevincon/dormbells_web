158bcc48f87f47be83c854bb80317e53bddafee3
Kevin Conley <kevincon@users.noreply.github.com>
2011-07-30 10:17:30 +0000

    another flush

--- generic_confirmation/main.py
+++
@@ -1,8 +0,0 @@
-
-# token format definitions
-# (<alphabet>, <length>)
-# alphabet here is without ijl10O which could easily be misread in some fonts
-#LONG = ('abcdefghkmnopqrstuvwwxyzABCDEFGHKLMNPQRSTUVWXYZ23456789', 24) # for emails
-#SHORT = ('abcdefghkmnopqrstuvwwxyzABCDEFGHKLMNPQRSTUVWXYZ23456789', 6) # because we can =)
-LONG = SHORT = SHORT_UPPER = ('ABCDEFGHKLMNPQRSTUVWXYZ23456789', 6) # for sms
-
