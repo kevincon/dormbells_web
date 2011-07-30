9ab304f1e0bd5e800aa2ae76cb9adc3c8b1f9aaf
Teddy Zhang <tianrun324@users.noreply.github.com>
2011-07-30 05:02:46 +0000

    Local setting file

---
+++ dormbells/local_setting.py
@@ -0,0 +1,16 @@
+import os.path
+
+PROJECT_ROOT = /Users/tlee/SVN/dormbells/
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dormbells',                      # Or path to database file if using sqlite3.
+        'USER': 'kevin',                      # Not used with sqlite3.
+        'PASSWORD': 'mysqlpass',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
