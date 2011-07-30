be131b92f0232498bd34d5922df7a38515133710
Kevin Conley <kevincon@users.noreply.github.com>
2011-07-30 09:59:40 +0000

    flush repo

--- dormbells/generic_confirmation/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from generic_confirmation.models import DeferredAction
-
-admin.site.register(DeferredAction, 
-            list_display=('token', 'valid_until', 'confirmed', 'is_expired'))
